7418a41490d1f5abba8bdfa531fc3056cbf63187
taziedevil <tazie_devil@hotmail.com>
2026-08-17 19:02:35 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.AUCTION_CONFIG = {
   supabaseUrl: "https://zcnwilkhmbezwckzyaem.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjbndpbGtobWJlendja3p5YWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NTMyNzQsImV4cCI6MjEwMjUyOTI3NH0.QNelxOVYq9w70F4xSu-1y2WisOzJb5_qB4lAo2fygis",
 
-  golferName: "Sam Rawlings",
-  golferDescription: "Blurb here",
+  golferName: "Dustin Crayford",
+  golferDescription: "I’m officially up for auction and looking for a team to take a chance on me! Bid generously, build your dream team, and help support a great cause at this 2026 REIT Mental Health Golf Day — where every swing counts, on and off the course.",
   golferPhoto: "golfer-placeholder.svg",
 
   startingBid: 100,
